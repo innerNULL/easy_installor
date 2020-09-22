823c4d037c35b3e7e85704ade7dc3dfae33a56ab
innerNULL <lyt697@outlook.com>
2020-09-22 19:17:21 +0800

    Update.

--- eastaller/opencv/install.sh
+++ eastaller/opencv/install.sh
@@ -4,13 +4,19 @@ set -x
 
 CURR_PATH_=$(pwd)
 GITHUB_PATH="opencv"
+QT_CMAKE_CONF="/usr/local/Cellar/qt/5.14.0/"
 
-git clone https://github.com/opencv/opencv.git
+#git clone https://github.com/opencv/opencv.git
 cd ${GITHUB_PATH}
 mkdir build && cd build
-cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local ../
+cmake \
+    -D CMAKE_BUILD_TYPE=Release \
+    -D CMAKE_INSTALL_PREFIX=${QT_CMAKE_CONF} \
+    -D WITH_QT=ON
 make -j7
 make install
+
+rm -rf build
 
 #rm -rf ${GITHUB_PATH}
 
