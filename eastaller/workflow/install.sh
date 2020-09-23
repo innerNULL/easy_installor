c09d43d6eac281289ba8cbbc66f4417ce5ef9687
innerNULL <lyt697@outlook.com>
2020-09-23 17:30:27 +0800

    Dev backup.

--- eastaller/workflow/install.sh
+++ eastaller/workflow/install.sh
@@ -11,7 +11,9 @@ git clone ${GITHUB_URL}
 cd ${GITHUB_PATH}
 mkdir build && cd build
 
-cmake -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl@1.1/include -DOPENSSL_LIBRARIES=/usr/local/opt/openssl@1.1/lib ../
+export OPENSSL_ROOT_DIR=/usr/local/opt/openssl
+cmake ../
+#cmake -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl@1.1/include -DOPENSSL_LIBRARIES=/usr/local/opt/openssl@1.1/lib ../
 
 make -j7
 make install
