13d0a33eb81144d25f392a15c768daa8b6df2572
innerNULL <lyt697@outlook.com>
2021-01-17 15:49:48 +0800

    Adds generalize dev base env building script.

---
+++ eastaller/dockerbuild/basic_dev_env/v0/build_env.sh
@@ -0,0 +1,53 @@
+# Author: innerNULL 
+# Date: 2021-01-17
+
+
+set -x
+
+apt-get update
+apt-get install -y --no-install-recommends \
+    cmake \
+    git \
+    zsh \
+    curl \
+    vim \
+    wget \
+    tar \
+    tmux \
+    ssh \
+    gcc g++ \
+    ca-certificates \
+    libssl-dev
+apt-get remove openssh-client openssh-server
+apt-get install openssh-client openssh-server
+service ssh restart
+rm -rf /var/lib/apt/lists/*
+
+chsh -s /usr/bin/zsh && zsh
+
+# config tmux
+touch ~/.tmux.conf && curl https://raw.githubusercontent.com/innerNULL/config4/main/config4/tmux/tmux.conf >> ~/.tmux.conf
+# Fix zsh tab completion duplicating command name bug
+echo DISABLE_AUTO_TITLE="true" >> ~/.zshrc
+# sync .zshrc
+cat ~/.bashrc >> ~/.zshrc
+# install oh-my-zsh
+sh -c "$(wget --no-check-certificate https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+# install spacevim
+#curl -sLf https://spacevim.org/cn/install.sh | bash
+
+# install python and pip
+apt-get update && apt-get -y install python3 python3-pip 
+#echo "alias python=python3" >> ~/.bashrc
+echo "alias python=python3" >> ~/.zshrc
+echo "alias pip=pip3" >> ~/.zshrc
+source ~/.zshrc
+
+# install necessary python libraries
+# TODO: Fix following `pip` bug
+pip install numpy \
+            pandas \
+            pytorch \
+            sklearn
+
+rm -rf ./*
