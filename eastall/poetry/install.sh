4b0ab0c4675940901bc3c5dda29a6f013459d1c3
innerNULL <lyt697@outlook.com>
2021-01-19 10:16:03 +0000

    Initial commit.

---
+++ eastall/poetry/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# file: install.sh
+# date: 2021-01-19
+
+
+set -x
+
+python -m pip install --user poetry
+
+echo "" >> ~/.bashrc
+echo "" >> ~/.bashrc
+echo "# Python poetry config" >> ~/.bashrc
+echo "alias poetry='python -m poetry'" >> ~/.bashrc
+echo "# Python poetry config end" >> ~/.bashrc
+
+source ~/.bashrc
