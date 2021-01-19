77956ba6d7d6018472eb44f41867f05a8c315858
innerNULL <lyt697@outlook.com>
2021-01-19 09:23:17 +0000

    Adds simple python installer.

---
+++ eastall/python/install.sh
@@ -0,0 +1,26 @@
+# file: install.sh
+# date: 2021-01-19
+
+
+set -x
+
+CURR_PATH=$(pwd)
+TMP_PATH="./_tmp"
+TARGET_VERSION="3.9.1"
+
+mkdir -p ${TMP_PATH} && cd ${TMP_PATH}
+wget https://www.python.org/ftp/python/${TARGET_VERSION}/Python-${TARGET_VERSION}.tgz
+tar -zxvf Python-${TARGET_VERSION}.tgz
+cd ./Python-${TARGET_VERSION} 
+./configure --prefix=/usr/local/python3 --with-ssl
+make -j4 && make install
+
+echo "" >> ~/.bashrc
+echo "" >> ~/.bashrc
+echo "# Python conf" >> ~/.bashrc
+echo "alias python='/usr/local/python3/bin/python3'" >> ~/.bashrc
+echo "alias pip='/usr/local/python3/bin/python3 -m pip'" >> ~/.bashrc
+echo "# Python conf end" >> ~/.bashrc
+
+source ~/.bashrc
+
