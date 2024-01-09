eea232e7f8222040aa4c919e417834768698a35b
innerNULL <lyt697@outlook.com>
2024-01-09 13:21:04 +0800

    feat: Simple HF common-voice dataset downloader

---
+++ quicmd/huggingface/dataset_download_common_voice.sh
@@ -0,0 +1,39 @@
+# file: dataset_download_common_voice.sh
+# date: 2023-01-09
+
+
+#set -x
+
+
+HOST="https://datasets-server.huggingface.co/first-rows?dataset=common_voice"
+LANG="zh-CN"
+SPLIT="test"
+OUTPUT_DIR="./_hf_common_voice_dataset"
+
+
+function init() {
+  rm -rf ${OUTPUT_DIR}
+  mkdir -p ${OUTPUT_DIR}/${SPLIT}
+}
+
+
+function main() {
+  init
+  cd ${OUTPUT_DIR}/${SPLIT}
+  curl -X GET "${HOST}&config=${LANG}&split=${SPLIT}" >> metadata.json
+
+  for row in $(cat metadata.json | jq -c '.rows[]'); do
+    #echo ${row}
+    local row_id=$(echo ${row} | jq '.row_idx')
+    local src_url=$(echo ${row} | jq '.row.audio[0].src')
+    local audio_type=$(echo "${src_url}" | awk -F'.' '{print $NF}' | sed 's/\"//g')
+    local sample_name=${SPLIT}_${LANG}_${row_id}.${audio_type}
+    wget -O ./${sample_name} ${src_url}
+    echo "Finished downloading ${src_url} to ${sample_name}"
+  done
+
+  echo "Finished"
+}
+
+
+main
